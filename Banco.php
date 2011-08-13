26a6f5c2377a32877b2e0fb2366281397216e105
Luigi <luigisant@gmail.com>
2011-08-13 16:18:09 -0300

    primeiro commit

---
+++ Banco.php
@@ -0,0 +1,83 @@
+<?php
+
+
+
+class Banco {
+
+
+	public function __construct(){
+		$this->db = new PDO("mysql:host=localhost;dbname=blog",'root','123456');
+	}
+
+	public function inserir($tabela,$dados) {
+	
+		foreach ($dados as $dado => $valor){
+			$data[] = "`$dado`";
+			$values[] = $valor;
+			$holders[] = "?";
+		}
+		
+	
+		$data = implode(',', $data);
+		$holders = implode(',', $holders);		
+		
+		
+		$query = "INSERT INTO $tabela ($data) VALUES ($holders)";
+		$statement = $this->db->prepare($query);
+		$statement->execute($values);
+	
+	}
+	
+	public function alterar($tabela, $colunas, $id) {
+		
+		foreach ($colunas as $coluna => $valor){
+			$set[] = "$coluna = ?";
+			$values[] = $valor;
+		}
+		
+		$values[] = $id;
+		
+		$set = implode(',', $set);
+		
+		$query = "UPDATE $tabela SET $set WHERE id = ?";
+		$statement = $this->db->prepare($query);
+		$statement->execute($values);
+		
+		
+	}
+
+	public function remover($tabela, $id) {
+		$query = "DELETE FROM $tabela WHERE id = ?";
+		$statement = $this->db->prepare($query);
+		$statement->execute(array($id));
+	}
+	
+	public function listar($tabela, $colunas, $onde = null) {
+		
+		
+		if ($colunas != '*'){
+			foreach ($colunas as $coluna){
+				$columns[] = "`$coluna`";
+			}
+		
+			$columns = implode(',', $columns);
+		} else {
+			$columns = $colunas;
+		}
+		
+//		$query = "SELECT $columns FROM $tabela WHERE id = ?";
+		$query = "SELECT $columns FROM $tabela";
+		
+		if($onde != null) {
+			$query .= " WHERE $onde";
+		}
+		
+		$statement = $this->db->prepare($query);
+		$statement->execute();
+		return $statement->fetchAll();
+	}
+
+}
+
+
+
---
+++ ler.php
@@ -0,0 +1,16 @@
+<?php
+
+$db = new PDO("mysql:host=localhost;dbname=tasklist",'root','123456');
+
+$statement = $db->prepare("SELECT * FROM tasks");
+$statement->execute();
+$tasks = $statement->fetchAll();
+
+foreach($tasks as $task) {
+?>
+	<!--<li><?php echo $task['task'] ?>&nbsp   <a href="./remover.php?id=<?php echo $task['id'] ?>">
+	//		<img src="./delete.png" alt="Example image" name="fig"></a></li> -->
+			
+	<li><?php echo $task['task'] ?>&nbsp<img src="./delete.png" class="remover" id="<?php echo $task['id'] ?>"></li>
+<?php
+}
