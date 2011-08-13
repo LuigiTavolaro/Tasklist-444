61e87c6210e6369c33a85ae0fcebac492524ca98
Luigi <luigisant@gmail.com>
2011-08-13 17:03:57 -0300

    Seguindo padrao Zend

--- ler.php
+++ ler.php
@@ -1,12 +1,12 @@
 <?php
 
-$db = new PDO("mysql:host=localhost;dbname=tasklist",'root','123456');
+$db = new PDO("mysql:host=localhost;dbname=tasklist", 'root', '123456');
 
 $statement = $db->prepare("SELECT * FROM tasks");
 $statement->execute();
 $tasks = $statement->fetchAll();
 
-foreach($tasks as $task) {
+foreach ($tasks as $task) {
 ?>
 	<!--<li><?php echo $task['task'] ?>&nbsp   <a href="./remover.php?id=<?php echo $task['id'] ?>">
 	//		<img src="./delete.png" alt="Example image" name="fig"></a></li> -->
--- remover.php
+++ remover.php
@@ -1,9 +1,9 @@
 <?php
 
-$db = new PDO("mysql:host=localhost;dbname=tasklist",'root','123456');
+$db = new PDO("mysql:host=localhost;dbname=tasklist", 'root', '123456');
 
-$id_remove = $_POST["id"];
+$idRemove = $_POST["id"];
 
 
 /* Delete all rows from the FRUIT table */
-$count = $db->exec("DELETE FROM tasks WHERE id = $id_remove");
+$count = $db->exec("DELETE FROM tasks WHERE id = $idRemove");
